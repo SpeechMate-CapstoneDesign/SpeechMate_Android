d67f79afe69722695a92b61b74567db541065cc5
choiminjun <mj010504@naver.com>
2025-07-21 22:10:03 +0900

    chore: 오타 수정

--- core/network/src/main/java/com/speech/network/source/AuthDataSource.kt
+++ core/network/src/main/java/com/speech/network/source/AuthDataSource.kt
@@ -1,9 +1,9 @@
 package com.speech.network.source
 
 import com.speech.network.model.auth.LoginKakaoResponse
-import com.speech.network.model.auth.SignupKakaoResponse
+import com.speech.network.model.auth.SignUpKakaoResponse
 
 interface AuthDataSource {
     suspend fun loginKakao(idToken : String) : Result<LoginKakaoResponse>
-    suspend fun signupKakao(idToken : String, skill : String) : Result<SignupKakaoResponse>
+    suspend fun signupKakao(idToken : String, skill : String) : Result<SignUpKakaoResponse>
 }
