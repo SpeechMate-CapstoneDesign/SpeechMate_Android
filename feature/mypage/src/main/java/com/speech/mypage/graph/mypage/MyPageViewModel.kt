ddba58276481e94e8b85df2f103746dd0d030a68
choiminjun <mj010504@naver.com>
2025-09-21 18:12:21 +0900

    fix: 스피치 추가해도 마이페이지에 반영 안되던 문제 해결

--- feature/mypage/src/main/java/com/speech/mypage/graph/mypage/MyPageViewModel.kt
+++ feature/mypage/src/main/java/com/speech/mypage/graph/mypage/MyPageViewModel.kt
@@ -10,9 +10,11 @@ import com.speech.common.util.suspendRunCatching
 import com.speech.domain.repository.SpeechRepository
 import com.speech.mypage.graph.mypage.MyPageSideEffect.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
@@ -22,12 +24,18 @@ class MyPageViewModel @Inject constructor(
     private val speechRepository: SpeechRepository,
 ) : ContainerHost<MyPageState, MyPageSideEffect>, ViewModel() {
     override val container = container<MyPageState, MyPageSideEffect>(MyPageState())
+    private val refreshTrigger = MutableStateFlow(false)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val cachedSpeechFeeds = refreshTrigger.flatMapLatest { speechRepository.getSpeechFeeds() }.cachedIn(viewModelScope)
+    private val deletedSpeechIds = MutableStateFlow<Set<Int>>(emptySet())
+
 
     init {
-        intent {
+        viewModelScope.launch {
             speechRepository.speechUpdateEvents.collect { event ->
                 when (event) {
-                    is SpeechRepository.SpeechUpdateEvent.SpeechAdded -> getSpeechFeeds()
+                    is SpeechRepository.SpeechUpdateEvent.SpeechAdded -> refreshTrigger.value = !refreshTrigger.value
                     is SpeechRepository.SpeechUpdateEvent.SpeechDeleted -> {
                         Log.d("speechUpdateEvent", "speech deleted: ${event.speechId})")
                         deletedSpeechIds.value += event.speechId
@@ -56,13 +64,10 @@ class MyPageViewModel @Inject constructor(
                 )
             }
 
-
             is MyPageIntent.OnDeleteClick -> onDeleteClick(event.speechId)
         }
     }
 
-    private val cachedSpeechFeeds = speechRepository.getSpeechFeeds().cachedIn(viewModelScope)
-    private val deletedSpeechIds = MutableStateFlow<Set<Int>>(emptySet())
 
     fun getSpeechFeeds() = intent {
         reduce {
