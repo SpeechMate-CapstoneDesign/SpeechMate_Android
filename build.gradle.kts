75a7c5297123dcbbb07f0e3911a569bd32c2262c
choiminjun <mj010504@naver.com>
2025-10-04 14:51:32 +0900

    feat: fireabse analytics, crashlytics 등록

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ktlint)
-    // alias(libs.plugins.google.services) apply false
-    // alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
 
 ktlint {
