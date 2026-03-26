cbb2f5e15652b1175e85313d3e0d6276ca3d2cbb
mj010504 <mj010504@naver.com>
2026-03-26 16:32:50 +0900

    refactor: 알림 권한 요청: MainActivty -> PracticeScreen으로 이동, 마이크/카메라/알림 -> 알림 권한 요청으로 축소

--- feature/main/src/main/java/com/speech/main/MainActivity.kt
+++ feature/main/src/main/java/com/speech/main/MainActivity.kt
@@ -1,54 +1,31 @@
 package com.speech.main
 
-import android.Manifest
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import androidx.lifecycle.compose.LifecycleStartEffect
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
 import com.example.designsystem.component.SpeechMateSnackBar
 import com.example.designsystem.component.SpeechMateSnackBarHost
-import com.kakao.sdk.common.util.Utility
 import com.speech.analytics.AnalyticsEvent
 import com.speech.analytics.AnalyticsEvent.PropertiesKeys.SCREEN_NAME
 import com.speech.analytics.AnalyticsEvent.Types.SCREEN_VIEW
 import com.speech.analytics.AnalyticsHelper
-import com.speech.auth.navigation.navigateToLogin
 import com.speech.common_ui.compositionlocal.LocalSnackbarHostState
 import com.speech.common_ui.ui.SpeechMateBottomBarAnimation
 import com.speech.designsystem.theme.SmTheme
@@ -64,7 +41,6 @@ import com.speech.navigation.shouldHideBottomBar
 import com.speech.practice.navigation.navigateToFeedback
 import com.speech.practice.navigation.navigateToPractice
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.compose.collectSideEffect
 import javax.inject.Inject
 
@@ -81,7 +57,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
-        requestPermissions(this)
         window.isNavigationBarContrastEnforced = false
 
         if(intent.extras != null) handleNotificationIntent(intent)
@@ -203,21 +178,6 @@ class MainActivity : ComponentActivity() {
             MyPageGraph.WebViewRoute::class
         )
     }
-}
-
-
-
-private fun requestPermissions(activity: Activity) {
-    val permissions = mutableListOf(
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.CAMERA,
-    )
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        permissions.add(Manifest.permission.POST_NOTIFICATIONS)
-    }
-
-    ActivityCompat.requestPermissions(activity, permissions.toTypedArray(), 1001)
 }
 
 
