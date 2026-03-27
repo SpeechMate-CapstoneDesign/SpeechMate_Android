939dd3fb2d352824f6d89931b816b411291e1c72
mj010504 <mj010504@naver.com>
2026-03-27 16:40:46 +0900

    chore: 1.0.3 -> 1.0.4로 버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
